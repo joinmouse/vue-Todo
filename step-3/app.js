7050f7edb41fc515ab17d9181c5da3af19caf2b5
joinmouse <908380434@qq.com>
2017-08-27 01:23:20 +0800

    sign

--- step-3/app.js
+++ step-3/app.js
@@ -1,9 +1,26 @@
 //import xxx from 'path' => 默认导入模块
 import Vue from 'vue'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'FH1m2mhIwPfYcOXTmjll4rC2-gzGzoHsz';
+var APP_KEY = 'H2c4tWeqKad4r7HD59UCwtM8';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
 
 var app = new Vue({
   el:'#app',
   data: {
+    actionType: 'signUp',
+    formData: {
+      username: '',
+      emial: '',
+      password: '',
+    },
+    currentUser: null,  ///LeanCloud文档: AV.User.current() 可以获取当前登录的用户
     newTodo: '',      //newTodo作为input的值
     todoList: []      //todoList数组作为所有待办事项的容器
   },
@@ -11,11 +28,29 @@ var app = new Vue({
     addTodo: function(){
       this.todoList.push({
         title: this.newTodo,
-        createdAt: new Date(),
         done: false  //添加done属性
       })
       this.newTodo = ''  //输入后变为空
+      this.saveOrUpdateTodos()
     },
+    saveOrUpdateTodos: function(){
+      if(this.todoList.id){
+        this.updateTodos()
+      }else{
+        this.saveTodos()
+      }
+    },
+    updateTodos: function(){
+      //JSON 在序列化这个有 id 的数组的时候，会得出怎样的结果？
+      let dataString = JSON.stringify(this.todoList)  
+      let avTodos = AV.Object.createWithoutData('AllTodos',this.todoList.id)
+      avTodos.set('content',dataString)
+      avTodos.save().then(()=>{
+        console.log('更新成功')
+      })
+    },
+
+
     removeTodo: function(todo){
       let index = this.todoList.indexOf(todo) // Array.prototype.indexOf 是ES5新加的 API
       this.todoList.splice(index,1)
@@ -23,11 +58,18 @@ var app = new Vue({
   },
   create:function(){
     window.onbeforeunload = ()=>{
-      let dataString = JSON.stringify(this.todoList) // JSON 文档: 
+      let dataString = JSON.stringify(this.todoList)   // JSON文档
       window.localStorage.setItem('myTodos', dataString) // localStorage文档
     }
-    let oldDataString = window.localStorage.getItem('myTodos')
-    let oldData = JSON.parse(oldDataString)
-    this.todoList = oldData || []
+    //检查用户是否登录
+    this.currentUser = this.getCurrentUser();
+
+    //获取User的Todos
+    this.fetchTodos()
+    
+    ////本地保存newTodo未发布内容
+    let uncompleteDataString = window.localStorage.getItem('typeTodo')
+    let uncompleteData = JSON.parse(uncompleteDataString)
+    this.newTodo = uncompleteData || []
   }
 })
